7292df71c996522bf544881e6e5cf527e4b7ebb7
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2025-03-04 21:43:17 +0530

    Update ordersApiSlice.js

--- src/slices/ordersApiSlice.js
+++ src/slices/ordersApiSlice.js
@@ -2,62 +2,61 @@ import { ORDERS_URL, RAZORPAY_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const ordersApiSlice = apiSlice.injectEndpoints({
-  endpoints: builder => ({
+  endpoints: (builder) => ({
     createOrder: builder.mutation({
-      query: order => ({
+      query: (order) => ({
         url: ORDERS_URL,
         method: 'POST',
         body: { ...order },
-        credentials:'include'
       }),
-      invalidatesTags: ['Order']
+      invalidatesTags: ['Order'],
     }),
+
     getOrderDetails: builder.query({
-      query: orderId => ({
+      query: (orderId) => ({
         url: `${ORDERS_URL}/${orderId}`,
-        credentials:'include'
       }),
-      providesTags: ['Order']
+      providesTags: ['Order'],
     }),
+
     getMyOrders: builder.query({
       query: () => ({
         url: `${ORDERS_URL}/my-orders`,
-        method:'GET',
-        credentials:'include'
       }),
-      providesTags: ['Order']
+      providesTags: ['Order'],
     }),
+
     payOrder: builder.mutation({
       query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
         method: 'PUT',
         body: { ...details },
-        credentials:'include'
       }),
-      invalidatesTags: ['Order']
+      invalidatesTags: ['Order'],
     }),
+
     updateDeliver: builder.mutation({
-      query: orderId => ({
+      query: (orderId) => ({
         url: `${ORDERS_URL}/${orderId}/deliver`,
         method: 'PUT',
-        credentials:'include'
       }),
-      invalidatesTags: ['Order']
+      invalidatesTags: ['Order'],
     }),
+
     getRazorpayApiKey: builder.query({
       query: () => ({
-        url: `${RAZORPAY_URL}/razorpay/config`
+        url: `${RAZORPAY_URL}/razorpay/config`,
       }),
-      providesTags: ['Order']
+      providesTags: ['Order'],
     }),
+
     getOrders: builder.query({
       query: () => ({
         url: ORDERS_URL,
-        credentials:'include'
       }),
-      providesTags: ['Order']
-    })
-  })
+      providesTags: ['Order'],
+    }),
+  }),
 });
 
 export const {
@@ -67,79 +66,5 @@ export const {
   useUpdateDeliverMutation,
   useGetRazorpayApiKeyQuery,
   useGetMyOrdersQuery,
-  useGetOrdersQuery
- } = ordersApiSlice;
-
-//  import { ORDERS_URL, RAZORPAY_URL } from '../constants';
-// import { apiSlice } from './apiSlice';
-
-// export const ordersApiSlice = apiSlice.injectEndpoints({
-//   endpoints: builder => ({
-//    createOrder: builder.mutation({
-//       query: order => ({
-//         url: ORDERS_URL,
-//         method: 'POST',
-//         body: { ...order },
-//        credentials:'include'
-//         // headers: {
-//         //   Authorization: `Bearer ${localStorage.getItem('token')}` // Include token from localStorage
-//         // }
-//       }),
-//       invalidatesTags: ['Order']
-//     }),
-//     getOrderDetails: builder.query({
-//       query: orderId => ({
-//         url: `${ORDERS_URL}/${orderId}`,
-//           credentials:'true'
-//       }),
-//       providesTags: ['Order']
-//     }),
-//     getMyOrders: builder.query({
-//       query: () => ({
-//         url: `${ORDERS_URL}/my-orders`,
-//         credentials:'include'
-//       }),
-//       providesTags: ['Order']
-//     }),
-//     payOrder: builder.mutation({
-//       query: ({ orderId, details }) => ({
-//         url: `${ORDERS_URL}/${orderId}/pay`,
-//         method: 'PUT',
-//         body: { ...details },
-//        credentials:'true'
-//       }),
-//       invalidatesTags: ['Order']
-//     }),
-//     updateDeliver: builder.mutation({
-//       query: orderId => ({
-//         url: `${ORDERS_URL}/${orderId}/deliver`,
-//         method: 'PUT',
-//        credentials:'true'
-//       }),
-//       invalidatesTags: ['Order']
-//     }),
-//     getRazorpayApiKey: builder.query({
-//       query: () => ({
-//         url: `${RAZORPAY_URL}/razorpay/config`
-//       }),
-//       providesTags: ['Order']
-//     }),
-//     getOrders: builder.query({
-//       query: () => ({
-//         url: ORDERS_URL,
-//        credentials:'true'
-//       }),
-//       providesTags: ['Order']
-//     })
-//   })
-// });
-
-// export const {
-//   useGetOrderDetailsQuery,
-//   useCreateOrderMutation,
-//   usePayOrderMutation,
-//   useUpdateDeliverMutation,
-//   useGetRazorpayApiKeyQuery,
-//   useGetMyOrdersQuery,
-//   useGetOrdersQuery
-// } = ordersApiSlice;
+  useGetOrdersQuery,
+} = ordersApiSlice;
